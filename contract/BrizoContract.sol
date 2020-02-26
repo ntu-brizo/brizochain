609d413e0819d359c5be869601ac5aed77367de1
VensonLiou <dewcoc-fetwuc-1wuFma>
2020-02-26 16:33:14 +0800

    update contract

--- contract/BrizoContract.sol
+++ contract/BrizoContract.sol
@@ -1,20 +1,35 @@
-pragma solidity ^0.5.11;
+pragma solidity 0.4.26;
 
 contract Brizo {
     mapping(address => string[]) storedData;
+    mapping(string => string) hashDict;
+    uint256 dataCounter = 0;
 
-    function dataCounter(address addr) public view returns (uint256) {
-        return storedData[addr].length;
+    function getCounter() public view returns (uint256) {
+        return dataCounter;
     }
-    
-    function writeData(string memory data) public {
-        require(bytes(data).length > 0, "The content of data is empty!");
-        storedData[msg.sender].push(data);
+
+    function writeData(string memory content) public {
+        require(bytes(content).length > 0, "The content of data is empty!");
+        storedData[msg.sender].push(content);
+        dataCounter += 1;
     }
-    
+
     function readData(address addr, uint index) public view returns (string memory) {
-        require(storedData[addr].length > 0, "No data was written by this address");
+        require(storedData[addr].length > 0, "No data was written by this address!");
         require((index >= 0) && (index < storedData[addr].length), "Index out of range!");
         return (storedData[addr][index]);
+    }
+
+    function writeDataToHashDict(string memory hashKey, string memory content) public {
+        require(bytes(hashKey).length > 0, "No hash key specified!");
+        require(bytes(content).length > 0, "Content is empty!");
+        hashDict[hashKey] = content;
+        dataCounter += 1;
+    }
+
+    function readDataFromHashDict(string hashKey) public view returns (string memory) {
+        require(bytes(hashDict[hashKey]).length > 0, "No data was written using this hash key!");
+        return (hashDict[hashKey]);
     }
 }
